b7c718609f707ff83462795f7377b24f6b270717
mgoel1 <mgoel@walmartlabs.com>
2020-07-18 15:16:15 -0700

    adding navigation

--- app/src/main/java/com/mukundgoel/memorynotes/presentation/ListFragment.kt
+++ app/src/main/java/com/mukundgoel/memorynotes/presentation/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.mukundgoel.memorynotes.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -19,5 +21,16 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addNote.setOnClickListener { goToNoteDetails() }
+    }
+
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action = ListFragmentDirections.actionGoToNote(id)
+        Navigation.findNavController(notesListView).navigate(action)
     }
 }
