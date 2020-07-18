a9ee348531c2d0ccb9224b2765e7bd505646aa1c
mgoel1 <mgoel@walmartlabs.com>
2020-07-18 14:54:27 -0700

    adding note fragment layout

---
+++ app/src/main/java/com/mukundgoel/memorynotes/presentation/NoteFragment.kt
@@ -0,0 +1,18 @@
+package com.mukundgoel.memorynotes.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mukundgoel.memorynotes.R
+
+class NoteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+}
