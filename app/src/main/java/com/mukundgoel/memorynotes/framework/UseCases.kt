f6e077f90643894d1ccbeb0d96aedbd946159918
mgoel1 <mgoel@walmartlabs.com>
2020-07-18 16:33:34 -0700

    adding use cases data class to be used for dependency injection later

---
+++ app/src/main/java/com/mukundgoel/memorynotes/framework/UseCases.kt
@@ -0,0 +1,16 @@
+package com.mukundgoel.memorynotes.framework
+
+import com.mukundgoel.core.usecase.AddNote
+import com.mukundgoel.core.usecase.GetAllNotes
+import com.mukundgoel.core.usecase.GetNote
+import com.mukundgoel.core.usecase.RemoveNote
+
+// allows us to instantiate our Use Cases and allows them to be used in our view model
+// allow us to inject our use cases where we will need them
+
+data class UseCases(
+    val addNote: AddNote,
+    val getAllNotes: GetAllNotes,
+    val getNote: GetNote,
+    val removeNote: RemoveNote
+)
